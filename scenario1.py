6d21a71ca070b8ed71b3101cf574af09b325a8e6
Alex <aliaksei.krulik@gmail.com>
2023-05-07 16:12:07 -0400

    Start the project and scenario 1

---
+++ scenario1.py
@@ -0,0 +1,4 @@
+# This is the test file for git version control system
+print("Scenario 1: Login to website.")
+
+# Successfully created project 'gitProgect' on GitHub, but initial commit failed: Author identity unknown *** Please tell me who you are. Run git config --global user.email "you@example.com" git config --global user.name "Your Name" to set your account's default identity. Omit --global to set the identity only in this repository. unable to auto-detect email address (got 'lexmo@MYPC.(none)'
