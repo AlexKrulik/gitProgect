cf3d4022b1d0e389ccbd04608ee031a5ded6b992
Alex <aliaksei.krulik@gmail.com>
2023-07-13 16:49:48 -0400

    commit2

--- test1.py
+++ test1.py
@@ -1,6 +1,22 @@
 
-#test text
 
-#new line 2
+class restaurant:
 
-#NEW LINE 3
+    def __init__(self, rest_name, rest_type):
+        self.name = rest_name
+        self.type = rest_type
+
+    def open_rest(self):
+        print(f'{self.name.title()} is open now')
+
+    def descr_rest(self):
+        print(f'Restaurant is {self.type.title()} cousin')
+
+restaurant1 = restaurant('1001', 'Uzbek')
+
+restaurant1.open_rest()
+restaurant1.descr_rest()
+print(restaurant1.name)
+restaurant1.name = '1001 Night'
+restaurant1.open_rest()
+
